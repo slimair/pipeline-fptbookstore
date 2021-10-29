ba3e8a95251601539e9ea26012be140475801daa
tiendvlp <tiendvlp@gmail.com>
2021-10-29 12:10:26 +0700

    init

---
+++ misc/jenkins.groovy
@@ -0,0 +1,84 @@
+def repo
+pipeline {
+    agent any
+    triggers {
+        pollSCM('* * * * *')
+    }
+    environment {
+      VERSION = '0.1'
+      GIT_COMMIT_SHORT = ''
+    }
+    options {
+      buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '10', daysToKeepStr: '', numToKeepStr: '10')
+    }
+    stages {
+      stage('Checkout') {
+        steps {
+          echo 'Checking out process'
+          dir('code') {
+            script {
+              repo = checkout([$class: 'GitSCM', branches: [[name: 'main']],
+                  userRemoteConfigs: [[url: 'https://github.com/haicao2805/online-book-management']]])
+                  GIT_COMMIT_SHORT = sh(
+                      script: "printf \$(git rev-parse --short ${repo.GIT_COMMIT})",
+                      returnStdout: true
+                  )
+            }
+            echo "Git commit short hash: ${GIT_COMMIT_SHORT}"
+          }
+        }
+      }
+      stage('Setup environment') {
+        steps {
+          sh '''
+            cp backend_config/app_setting.json code/FptBookStore
+          '''
+        }
+      }
+      stage('Build Image') {
+        steps {
+            sh """
+            docker stop prndotnet || true
+            docker rmi \$(docker images | grep 'tiendvlp/prndotnet') || true
+            docker build -f misc/Dockerfile -t tiendvlp/prndotnet:${GIT_COMMIT_SHORT} .
+            docker build -f misc/Dockerfile -t tiendvlp/prndotnet:latest .
+            """
+        }
+      }
+      stage('Publish') {
+        steps {
+          script {
+              withDockerRegistry([credentialsId: 'docker-hub', url: '' ]) {
+                sh """
+                      docker push tiendvlp/prndotnet:${GIT_COMMIT_SHORT}
+                      docker rmi tiendvlp/prndotnet:${GIT_COMMIT_SHORT}
+                      docker push tiendvlp/prndotnet:latest
+                """
+              }
+          }
+        }
+      }
+      stage('Run') {
+        steps {
+          sh """
+            docker run -d --rm --network MASA -p 8888:80 --name prndotnet tiendvlp/prndotnet:latest
+          """
+        }
+      }
+    }
+    post {
+      always {
+          echo 'Clean up workspace'
+          cleanWs deleteDirs: true
+      }
+      changed {
+          emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) ${currentBuild.result}",
+                body: "Please go to ${BUILD_URL} and verify it.", 
+                attachLog: true, 
+                compressLog: true, 
+                from: 'Slimair.System',
+                to: 'test@gmail',
+                recipientProviders: [upstreamDevelopers(), requestor(), buildUser()] 
+      }
+    }
+}
