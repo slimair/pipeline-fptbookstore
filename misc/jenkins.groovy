06593ed54e471f1184f7532af27537208012ada3
Dang Minh Tien <tiendvlp@gmail.com>
2021-11-05 12:18:37 +0700

    Update jenkins.groovy

--- misc/jenkins.groovy
+++ misc/jenkins.groovy
@@ -66,6 +66,20 @@ pipeline {
         }
       }
     }
+    stage('Functional testing') {
+            steps {
+             
+                    sh 'docker run -t --rm -v "$(pwd)":/tmp/project katalonstudio/katalon katalonc.sh -projectPath=/tmp/project -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
+                
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'Reports/**/*.*', fingerprint: true
+            junit 'Reports/**/JUnit_Report.xml'
+        }
+    }
     post {
       always {
           echo 'Clean up workspace'
