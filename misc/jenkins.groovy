bcf50a7bc338232ffefe55ce0158fa9c5dc5f365
tiendvlp <tiendvlp@gmail.com>
2021-11-05 16:22:35 +0700

    edit jenkins file

--- misc/jenkins.groovy
+++ misc/jenkins.groovy
@@ -73,8 +73,7 @@ pipeline {
                 repo = checkout([$class: 'GitSCM', branches: [[name: 'main']],
                     userRemoteConfigs: [[url: 'https://github.com/phuongnguyen521/SWT301-Katalon-FptBook']]])
               }
-              sh 'docker build -t tiendvlp/katalon .'
-              sh 'docker run tiendvlp/katalon'
+              sh 'docker-compose up'
               // sh 'docker run -t -v "$(pwd)":/tmp/project katalonstudio/katalon katalonc.sh -projectPath=/tmp/project/ -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey=ba490008-3999-4890-848b-b43048e5ca92'
             }
           }
@@ -82,6 +81,7 @@ pipeline {
     }
     post {
       always {
+          // docker-compose down
           echo 'Clean up workspace'
           // cleanWs deleteDirs: true
       }
