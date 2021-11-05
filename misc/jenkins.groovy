8aa8dcf6a130123c6377692cd318ed0694778d9c
tiendvlp <tiendvlp@gmail.com>
2021-11-05 13:39:36 +0700

    add katalon api key

--- misc/jenkins.groovy
+++ misc/jenkins.groovy
@@ -73,13 +73,7 @@ pipeline {
                 repo = checkout([$class: 'GitSCM', branches: [[name: 'main']],
                     userRemoteConfigs: [[url: 'https://github.com/phuongnguyen521/SWT301-Katalon-FptBook']]])
               }
-              sh 'docker run -t --rm -v "$(pwd)":/tmp/project katalonstudio/katalon katalonc.sh -projectPath=/tmp/project -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
-            }
-          }
-          post {
-            always {
-                archiveArtifacts artifacts: 'Reports/**/*.*', fingerprint: true
-                junit 'Reports/**/JUnit_Report.xml'
+              sh 'docker run -t --rm -v "$(pwd)":/tmp/project katalonstudio/katalon katalonc.sh -projectPath=/tmp/project -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey=ba490008-3999-4890-848b-b43048e5ca92'
             }
           }
       }
