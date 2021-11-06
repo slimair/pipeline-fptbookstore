7423bcc3507012a52b8eac0cc036b4867c5d5aa9
tiendvlp <tiendvlp@gmail.com>
2021-11-06 23:49:18 +0700

    a

--- misc/jenkins.groovy
+++ misc/jenkins.groovy
@@ -120,7 +120,7 @@ pipeline {
       stage('Run') {
         steps {
           sh """
-            docker stop prndotnet || true
+            docker rm -f prndotnet || true
             docker run -d --rm -v '/FptBook/image:/app/wwwroot/image' --network MASA -p 8888:80 --name prndotnet tiendvlp/prndotnet:latest
           """
         }
