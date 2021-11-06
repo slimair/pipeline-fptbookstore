22df51037ed843429eab45a0da267b98810f48cc
tiendvlp <tiendvlp@gmail.com>
2021-11-06 11:40:51 +0700

    use inside cmd

--- misc/jenkins.groovy
+++ misc/jenkins.groovy
@@ -86,7 +86,15 @@ pipeline {
       }
       stage ('Functional testing') {
         steps {
-          dir('katalon') {    
+            script {
+              def katalonStudio = docker.image('katalonstudio/katalon');
+              katalonStudio.pull();
+              katalonStudio.inside {
+                sh '''
+                  cd katalon
+                  ./katalonc -projectPath=$(pwd)/fptbookstore_katalon.prj -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/FptBook_TestSuite" -apiKey=ba490008-3999-4890-848b-b43048e5ca92 --config -webui.autoUpdateDrivers=true --allowed-ips="137.184.131.91" --disable-dev-shm-usage  --no-sandbox
+                '''
+              }
               sh 'docker-compose up --build'
           }
         }
