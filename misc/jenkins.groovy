92ebe06686f58f27de6c32001d1a13d1b9c8111e
tiendvlp <tiendvlp@gmail.com>
2021-11-06 11:47:00 +0700

    fixbug

--- misc/jenkins.groovy
+++ misc/jenkins.groovy
@@ -92,7 +92,7 @@ pipeline {
               katalonStudio.inside {
                 sh '''
                   cd katalon
-                  ./katalonc.sh -projectPath=$(pwd)/fptbookstore_katalon.prj -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/FptBook_TestSuite" -apiKey=ba490008-3999-4890-848b-b43048e5ca92 --config -webui.autoUpdateDrivers=true --allowed-ips="137.184.131.91" --disable-dev-shm-usage  --no-sandbox
+                  katalonc.sh -projectPath=$(pwd)/fptbookstore_katalon.prj -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/FptBook_TestSuite" -apiKey=ba490008-3999-4890-848b-b43048e5ca92 --config -webui.autoUpdateDrivers=true --allowed-ips="137.184.131.91" --disable-dev-shm-usage  --no-sandbox
                 '''
               }
               sh 'docker-compose up --build'
